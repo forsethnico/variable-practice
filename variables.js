651684d3d9f98ab2987f9f8f59467f8294014fe9
Nicole Forseth <forseth.nico@gmail.com>
2022-03-29 20:52:37 -0600

    Add three numbers and two boolean variables

--- variables.js
+++ variables.js
@@ -1,3 +1,10 @@
 var myName = "Nicole Forseth"
 var phoneNumber = "(608)317-5454"
-var favoriteColor = "green"
+var houseColor = "green"
+
+var yearHouseBuilt = 1987
+var houseNumber = 2760
+var housePrice = 250000
+
+var onMarket = true
+var hoafee = true
